986b9acc2c8e92c0be03543fc9895f0568ef7248
Travis K <traviskean04@live.com>
2015-08-25 11:43:50 -0400

    Changed Default Route, added continue as guest

--- DetroitEatz/App_Start/RouteConfig.cs
+++ DetroitEatz/App_Start/RouteConfig.cs
@@ -13,10 +13,16 @@ namespace DetroitEatz
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+            
             );
         }
     }
